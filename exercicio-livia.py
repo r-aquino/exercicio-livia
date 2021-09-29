1be22164a577299ba7dabd615362893f4fbc17bf
Rafael Aquino <rjoseaquino@gmail.com>
2021-09-28 22:49:38 -0300

    primeiro commit

---
+++ exercicio-livia.py
@@ -0,0 +1,37 @@
+# numero = int(input("Digite um número: "))
+# print("O antecessor é:",numero -1)
+
+# largura, altura = int(input("Digite a largura em m: ")), int(input("Digite a altura em m: "))
+# area = largura * altura
+# tinta = area / 2
+# print(tinta)
+
+# numero = int(input("Digite um número entre 1 e 10: "))
+
+# while numero > 10 or numero < 1:
+#     print("Número inválido!")
+#     numero = int(input("Digite novamente um número entre 1 e 10: "))
+# else: print("Ok, o número digitado foi: ",numero)
+
+
+# pesos = []
+# for n in range(1,6):
+#     pesos.append(input(f"Insira o peso da pessoa {n}: "))
+#     a = int(n)
+# print("O peso máximo foi: ", max(pesos))
+# print("O peso mínimo foi: ", min(pesos))
+# print("A média de peso é: ",  sum(pesos)/a)
+
+# import random
+# lista = ["Aline", "Bruna", "Gustavo", "Huan",
+# "Marlon", "Rafa", "Rafael", "Victor"]
+# sorteio = random.choice(lista)
+# print("O Sorteado foi", sorteio)
+
+
+# tupla = ('zero', 'um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez')
+# numero = int(input("Insira um numero de 0 a 10: "))
+# while numero < 0 or numero > 10:
+#     numero = int(input("Insira um numero de 0 a 10: "))    
+# print("O número foi o", tupla[numero])
+
